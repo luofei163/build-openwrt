32edfc0575d278ddc975efe60b66979470f3a8b3
root <root@wolf.cn>
2022-05-31 22:39:28 +0800

    test

--- first.sh
+++ first.sh
@@ -8,6 +8,8 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
+pwd
+ls
 # Remove old theme 
 rm -rf package/lean/luci-theme-argon
 
