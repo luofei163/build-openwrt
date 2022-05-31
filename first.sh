a5ebe4d288680a1b74b77c586c1a3597f9a147cc
root <root@wolf.cn>
2022-05-31 22:55:51 +0800

    Fixed theme installed failed

--- first.sh
+++ first.sh
@@ -8,13 +8,8 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-pwd
-ls
 # Remove old theme 
 rm -rf package/lean/luci-theme-argon
 
-# Download theme
+# Download argon theme
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-# Replace default theme to luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
--- second.sh
+++ second.sh
@@ -1,2 +1,6 @@
+
+# Replace default theme to luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
