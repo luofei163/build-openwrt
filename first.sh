ea62507d251213f3b8fa0674c8320a3c84841425
root <root@wolf.cn>
2022-06-01 17:59:31 +0800

    Update first.sh and installed firstwall by  default

--- first.sh
+++ first.sh
@@ -4,13 +4,16 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git unblockneteasemusic  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git' >>feeds.conf.default
+echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+
+echo 'src-git PWpackages unblockneteasemusic  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git;packages' >>feeds.conf.default
+echo 'src-git PWluci unblockneteasemusic  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git;luci' >>feeds.conf.default
 
 # Remove old theme 
 rm -rf package/lean/luci-theme-argon
 
 # Download argon theme
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b v2.2.9.4 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
